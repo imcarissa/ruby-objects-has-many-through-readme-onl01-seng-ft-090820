692918fd8faa3ba8353cb6b2a27a40d554396b3e
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-25 16:29:44 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -35,4 +35,15 @@ class Customer
     Meal.new(waiter, self, total, tip)
   end
     
+    def self.oldest_customer
+      oldest_age = 0 
+      oldest_customer = nil 
+      self.all.each do |customer|
+        if customer.age > oldest_age
+          oldest_age = customer.age
+          oldest_customer = customer
+        end
+      end
+      oldest_customer
+    end
 end
