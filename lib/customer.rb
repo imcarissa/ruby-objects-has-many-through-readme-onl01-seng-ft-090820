9b7b1394be88b5ba4e19202de1b1ee7736ae4718
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-25 16:28:03 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,10 +13,6 @@ class Customer
   def self.all 
     @@all
   end
-    
-  def new_meal(waiter, total, tip=0)
-    Meal.new(waiter, self, total, tip)
-  end
   
   def meals
     Meal.all.select do |meal|
@@ -31,6 +27,11 @@ class Customer
   end
   
   def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+  
+  def new_meal_20_percent(waiter, total)
+    tip = total * 0.2
     Meal.new(waiter, self, total, tip)
   end
     
